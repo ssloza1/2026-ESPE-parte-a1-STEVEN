d41912d3f3d621655e9b5aa628d5aab0e9726bb7
Steven <ssloza1@espe.edu.ec>
2026-01-22 09:54:29 -0500

    Update Experimental-UnitTest

--- src/main/java/es/upm/grise/profundizacion/cruiseControl/CruiseControl.java
+++ src/main/java/es/upm/grise/profundizacion/cruiseControl/CruiseControl.java
@@ -7,9 +7,7 @@ public class CruiseControl {
 	private Integer speedSet;
 	private Integer speedLimit;
 
-	/*
-	 * Constructor
-	 */
+	
 	public CruiseControl(Speedometer speedometer) {
 		this.speedometer = speedometer;
 		this.speedSet = null;
@@ -18,16 +16,14 @@ public class CruiseControl {
 	
 	
 	
-	/*
-	 * Method to code / test
-	 */
+	
 	public void setSpeedSet(int speedSet) throws IncorrectSpeedSetException, SpeedSetAboveSpeedLimitException {
-		// Check if speedSet is positive (strictly greater than zero)
+		
 		if (speedSet <= 0) {
 			throw new IncorrectSpeedSetException("Speed set must be strictly greater than zero");
 		}
 		
-		// Check if speedLimit is set and speedSet exceeds it
+		
 		if (speedLimit != null && speedSet > speedLimit) {
 			throw new SpeedSetAboveSpeedLimitException("Speed set cannot exceed speed limit");
 		}
@@ -37,9 +33,7 @@ public class CruiseControl {
 	
 	
 
-	/*
-	 * Other setters & getters
-	 */
+	
 	public Integer getSpeedLimit() {
 		
 		return speedLimit;
--- src/main/java/es/upm/grise/profundizacion/cruiseControl/Speedometer.java
+++ src/main/java/es/upm/grise/profundizacion/cruiseControl/Speedometer.java
@@ -2,9 +2,7 @@ package es.upm.grise.profundizacion.cruiseControl;
 
 public class Speedometer {
 	
-	// Please notice the difference between the class diagram and this implementation
-	// The reason is to facilitate unit testing
-	
+
 	int currentSpeed;
 	
 	void setCurrentSpeed(int currentSpeed) {
